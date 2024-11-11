f64e54b28819f69ceabaf0cf308e3dc3f29f94f2
klintat <98770096+klintat@users.noreply.github.com>
2024-11-11 12:48:01 +0200

    Created InputField component for reusable inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import "./App.scss";
-import PersonalDetails from "./components/PersonalDetails.jsx";
-import EducationalExperience from "./components/EducationalExperience.jsx";
-import WorkExperience from "./components/WorkExperience.jsx";
 
 function App() {
   return (
     <>
       <div>
         <h1>CV application</h1>
-        <PersonalDetails />
-        <EducationalExperience />
-        <WorkExperience />
+        
       </div>
     </>
   );
---
+++ src/components/InputField.jsx
@@ -0,0 +1,19 @@
+function InputField({ label, name, type, value, onChange, error }) {
+    return (
+        <div style={{ marginBottom: "10px"}}>
+            <label>
+                {label}:
+                <input 
+                    type={type}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    style={{ display: "block", marginTop: "5px"}}
+                />
+            </label>
+            {error && <p style={{ color: "red"}}>{error}</p>}
+        </div>
+    )
+}
+
+export default InputField
